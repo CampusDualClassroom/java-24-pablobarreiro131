9a4d92fcc711b84d70a4db9339b3e13d0657c99f
Pablo <pablobarreiro131@gmail.com>
2025-05-23 14:04:22 +0200

    Ejercicio 24, completado y testeado.

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -1,18 +1,26 @@
 package com.campusdual.classroom;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
+        Queue<String> cola = new LinkedList<>();
+        cola.add("Smith");
+        cola.add("Montessori");
+        cola.add("Peralta");
+        cola.add("House");
+
+        return cola;
 
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
-
+        System.out.println(queue.toString());
+        queue.removeAll(queue);
     }
 
     public static void main(String[] args) {
-
+        Queue<String> cola = createQueue();
+        printAndEmptyQueue(cola);
     }
 
 }
